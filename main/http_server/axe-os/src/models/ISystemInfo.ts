skot/ESP-Miner

update frontend to use camelcased api

main/http_server/axe-os/src/models/ISystemInfo.ts
@@ -2,14 +2,14 @@ import { eASICModel } from './enum/eASICModel';
 
 export interface ISystemInfo {
 
-    flipscreen: number;
-    invertscreen: number;
+    flipScreen: number;
+    invertScreen: number;
     power: number,
     voltage: number,
     current: number,
     temp: number,
     vrTemp: number,
-    hashRate: number,
+    hashrate: number,
     bestDiff: string,
     bestSessionDiff: string,
     freeHeap: number,
@@ -23,7 +23,7 @@ export interface ISystemInfo {
     uptimeSeconds: number,
     asicCount: number,
     smallCoreCount: number,
-    ASICModel: eASICModel,
+    asicModel: eASICModel,
     stratumURL: string,
     stratumPort: number,
     fallbackStratumURL: string,
@@ -35,13 +35,13 @@ export interface ISystemInfo {
     version: string,
     idfVersion: string,
     boardVersion: string,
-    invertfanpolarity: number,
-    autofanspeed: number,
-    fanspeed: number,
-    fanrpm: number,
+    invertFanPolarity: number,
+    autoFanSpeed: number,
+    fanSpeed: number,
+    fanRPM: number,
     coreVoltageActual: number,
 
     boardtemp1?: number,
     boardtemp2?: number,
-    overheat_mode: number
+    overheatMode: number
 }

aaaaaaaaaaaaaalex/miner

main/http_server/axe-os/src/models/ISystemInfo.ts
@@ -8,8 +8,8 @@ interface ISharesRejectedStat {
 export interface ISystemInfo {
 
     display: string;
-    flipscreen: number;
-    invertscreen: number;
+    flipScreen: number;
+    invertScreen: number;
     displayTimeout: number;
     power: number,
     voltage: number,
@@ -18,7 +18,7 @@ export interface ISystemInfo {
     vrTemp: number,
     maxPower: number,
     nominalVoltage: number,
-    hashRate: number,
+    hashrate: number,
     expectedHashrate: number,
     bestDiff: string,
     bestSessionDiff: string,
@@ -35,7 +35,7 @@ export interface ISystemInfo {
     uptimeSeconds: number,
     asicCount: number,
     smallCoreCount: number,
-    ASICModel: eASICModel,
+    asicModel: eASICModel,
     stratumURL: string,
     stratumPort: number,
     fallbackStratumURL: string,
@@ -47,15 +47,15 @@ export interface ISystemInfo {
     version: string,
     idfVersion: string,
     boardVersion: string,
-    autofanspeed: number,
-    fanspeed: number,
-    temptarget: number,
-    fanrpm: number,
+    autoFanSpeed: number,
+    fanSpeed: number,
+    tempTarget: number,
+    fanRPM: number,
     coreVoltageActual: number,
 
     boardtemp1?: number,
     boardtemp2?: number,
-    overheat_mode: number,
-    power_fault?: string
+    overheatMode: number,
+    powerFault?: string
     overclockEnabled?: number
 }